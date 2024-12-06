04d31b9e03cd7cb7194cf1aeee6421b7ffa6e751
maanasp <maanasp@umich.edu>
2024-12-05 22:20:09 -0500

    oops broke smthn fixed it

--- functions.py
+++ functions.py
@@ -49,12 +49,10 @@ def grid_in_plane(origin, normal, spacing, plane_size):
     Generates a grid of points in a plane defined by an origin and a normal vector.
     Note that the grid is centered at the origin and the points are spaced the same in both in-plane directions.
     You want to make sure the grid is large enough to cover the entire image (plane_size argument).
-
     Args:
         origin (numpy.ndarray): A 3-element numpy array representing the coordinates of the origin point of the plane.
         normal (numpy.ndarray): A 3-element numpy array representing the normal vector of the plane.
         plane_size (float): The size of the plane in pixel units
-
     Returns:
         numpy.ndarray: A 2D array of shape (N, 3) where each row represents the coordinates (in pixel units) of a point in the plane.
     """
@@ -123,12 +121,10 @@ def grid_in_plane2(origin, normal, spacing, plane_size):
     Generates a grid of points in a plane defined by an origin and a normal vector.
     Note that the grid is centered at the origin and the points are spaced the same in both in-plane directions.
     You want to make sure the grid is large enough to cover the entire image (plane_size argument).
-
     Args:
         origin (numpy.ndarray): A 3-element numpy array representing the coordinates of the origin point of the plane.
         normal (numpy.ndarray): A 3-element numpy array representing the normal vector of the plane.
         plane_size (float): The size of the plane in pixel units
-
     Returns:
         numpy.ndarray: A 2D array of shape (N, 3) where each row represents the coordinates (in pixel units) of a point in the plane.
     """
@@ -168,11 +164,9 @@ def grid_in_plane2(origin, normal, spacing, plane_size):
 def interpolate_image(xyz, data, ct_affine):
     """
     Interpolates the given image data based on the provided coordinates.
-
     Args:
         xyz (tuple of float): The coordinates in the form of (x, y, z) that need to be converted to integer indices.
         data (numpy.ndarray): The image data to be interpolated.
-
     Returns:
         numpy.ndarray: The interpolated image data with integer values.
     """
@@ -285,12 +279,10 @@ def plot_short_axis(label_data, affine, lv_centroid, lv_long_axis, plane_size=10
         slice_origin = lv_centroid + (sliceIndex - number_of_slices // 2) * out_of_plane_spacing * lv_long_axis # print(f"Slice {sliceIndex} Origin: {slice_origin}")
         xyz, slice_affine = grid_in_plane(slice_origin, lv_long_axis, spacing, plane_size)                      # print(f"Slice {sliceIndex}: xyz Grid = {xyz[:5]}") 
         slice_affine[:3, 3] = slice_origin
+        # slice_affine[:3, 3] = slice_origin
         slice_affines.append(slice_affine)
         slice_data = interpolate_image(xyz, label_data, affine)
         slices.append(slice_data)
-        
-    # Save each slice's affine alongside the data? 
-    # for loop and save each affine as its own affine?
 
     slices_3d = np.stack(slices, axis=0)
     middle_affine = slice_affines[number_of_slices // 2]
@@ -311,6 +303,7 @@ def plot_short_axis(label_data, affine, lv_centroid, lv_long_axis, plane_size=10
         plt.show()
 
     return slices_3d, slice_affines
+    return slices_3d, slice_affines[0]
 
 def plot_2_chamber_view(label_data, affine, lv_centroid, rv_centroid, plane_size=100, spacing=1.0, 
                         out_of_plane_spacing=8.0, number_of_slices=13, plotOn = True):
@@ -328,6 +321,7 @@ def plot_2_chamber_view(label_data, affine, lv_centroid, rv_centroid, plane_size
         slice_origin = lv_centroid + (slice_index - number_of_slices // 2) * out_of_plane_spacing * v  # print(f"Slice {sliceIndex} Origin: {slice_origin}")
         xyz, slice_affine = grid_in_plane(slice_origin, v, spacing, plane_size) # print(f"Slice {sliceIndex}: xyz Grid = {xyz[:5]}") 
         slice_affine[:3, 3] = slice_origin
+        # slice_affine[:3, 3] = slice_origin
         slice_data = interpolate_image(xyz, label_data, affine)
         slices.append(slice_data)
         slice_affines.append(slice_affine)
@@ -342,6 +336,7 @@ def plot_2_chamber_view(label_data, affine, lv_centroid, rv_centroid, plane_size
         plt.show()
 
     return slices_3d, slice_affine
+    return slices_3d, slice_affines[0]
 
 
 def main_2chamber_view(seg_file, plane_size=100, spacing=1.0, out_of_plane_spacing=8.0, number_of_slices=13, plotOn = True):
@@ -369,7 +364,7 @@ def plot_4_chamber_view(label_data, affine, lv_centroid, rv_centroid, lv_long_ax
     if lv_centroid is None or rv_centroid is None:
         print("Invalid LV or RV centroid.")
         return None, None
-    
+
     slices = []
     slice_affines = []
     v = rv_centroid - lv_centroid
@@ -381,6 +376,7 @@ def plot_4_chamber_view(label_data, affine, lv_centroid, rv_centroid, lv_long_ax
         slice_origin = lv_centroid + (slice_index - number_of_slices // 2) * out_of_plane_spacing * x
         xyz, slice_affine = grid_in_plane(slice_origin, x, spacing, plane_size)
         slice_affine[:3, 3] = slice_origin
+        # slice_affine[:3, 3] = slice_origin
         slice_data = interpolate_image(xyz, label_data, affine)
         slices.append(slice_data)
         slice_affines.append(slice_affine)
@@ -395,6 +391,7 @@ def plot_4_chamber_view(label_data, affine, lv_centroid, rv_centroid, lv_long_ax
         plt.show()
 
     return slice_data, slice_affines
+    return slices_3d, slice_affines[0]
 
 def main_4chamber_view(seg_file, plane_size=100, spacing=1.0, out_of_plane_spacing = 8.0, number_of_slices = 13, plotOn = True):
     label_data, affine, _ = readFromNIFTI(seg_file)
@@ -437,6 +434,7 @@ def plot_3_chamber_view(label_data, affine, lv_centroid, aorta_centroid, lv_long
         slice_origin = lv_centroid + (slice_index - number_of_slices // 2) * out_of_plane_spacing * normal
         xyz, slice_affine = grid_in_plane(slice_origin, normal, spacing, plane_size)
         slice_affine[:3, 3] = slice_origin
+        # slice_affine[:3, 3] = slice_origin
         slice_data = interpolate_image(xyz, label_data, affine)
         slices.append(slice_data)
         slice_affines.append(slice_affine)
@@ -451,6 +449,7 @@ def plot_3_chamber_view(label_data, affine, lv_centroid, aorta_centroid, lv_long
         plt.show()
 
     return slices_3d, slice_affines
+    return slices_3d, slice_affines[0]
 
 
 def main_3chamber_view(seg_file, plane_size=100, spacing=1.0, out_of_plane_spacing=8.0, number_of_slices=13, plotOn=True):
@@ -565,3 +564,45 @@ def display_views(sa_data=None, la_2CH_data=None, la_3CH_data=None, la_4CH_data=
 
     plt.tight_layout()
     plt.show()
+import plotly.graph_objects as go
+import plotly.io as pio
+def show_point_cloud(points, fig=None, color=None, size=10, cmap='Viridis',
+                     opacity=1, marker_symbol='circle', label=None, showscale=False,
+                     cmin = None, cmax = None):
+    if fig is None:
+        fig = go.Figure()
+    if len(points.shape) == 1:
+        points = points[None]
+    fig.add_scatter3d(x=points[:,0], y=points[:,1], z=points[:,2], mode='markers',
+                      marker=dict(
+                                    color=color,
+                                    size=size,
+                                    colorscale=cmap,
+                                    opacity=opacity,
+                                    symbol=marker_symbol,
+                                    showscale=showscale,
+                                    cmin = cmin,
+                                    cmax = cmax
+                                ),
+                      name = label
+        )
+    return fig
+
+    
+def show_segmentations(data, affine, fig=None, background=False):
+    nlabels = np.unique(data)
+    nlabels = nlabels[nlabels != 0]
+    colors = {0: 'blue', 1: 'red', 2: 'green', 3: 'cyan',
+              4: 'grey', 5: 'yellow', 6: 'purple', 7: 'magenta',
+              8: 'aqua', 9: 'maroon', 10: 'olive', 11: 'lime'}
+    
+    if fig is None:
+        fig = go.Figure()
+    for i, label in enumerate(nlabels):
+        mask = data == label
+        ijk = np.vstack(np.where(mask)).T
+        xyz = nib.affines.apply_affine(affine, ijk)
+        show_point_cloud(xyz, fig=fig, opacity=0.9, color=colors[i], size=5)
+    if not background:
+        fig.update_scenes(xaxis_visible=False, yaxis_visible=False,zaxis_visible=False )
+    return fig
